a5b94a4d3ecb8e0ccf9a126187ead60551b0dd68
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-09-04 09:48:09 -0300

    Se termino de estilziar el navbar y se añadio la barra de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Header } from "./components/header/Header";
 import Layout from "./components/layout/Layout";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Layout>
+      <Header />
       <AppRoutes />
     </Layout>
+  );
+}
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,21 @@
+import { Search } from "../searchComponent/Search";
+import logoMain from "/public/header-img/logoMain.png";
 export const Header = () => {
   return (
     <header className="header bg-black py-4 px-8">
-      <nav className="flex justify-between w-full h-[50px] items-center px-4">
-        <img src="" alt="IMAGEN FALSA BRO" className="h-8" />
-        <ul className="flex gap-8 cursor-pointer p-4">
-          <li className="nav-item text-white font-semibold px-6 py-2">
-            Soporte
-          </li>
-          <li className="nav-item text-white font-semibold px-6 py-2">
-            Registrarse/Ingresar
-          </li>
-        </ul>
+      <nav className="flex flex-col items-center justify-between w-full">
+        <div className="flex items-center justify-between w-full gap-8">
+          <img src={logoMain} alt="Logo" className="h-12 w-auto" />
+          <Search />
+          <ul className="flex gap-8 cursor-pointer items-center">
+            <li className="nav-item text-white font-semibold px-6 py-2 text-[16px]">
+              Soporte
+            </li>
+            <li className="nav-item text-white font-semibold px-6 py-2 text-[16px]">
+              Registrarse/Ingresar
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
