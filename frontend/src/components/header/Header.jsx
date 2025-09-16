53b1e2eb27a7b3f118552cc7cf379470b4c26778
Laureano <laureanojuarez69@gmail.com>
2025-09-16 09:35:51 -0300

    feat: add FormPage component and update routing for support page

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,14 +1,21 @@
 import logoMain from "/header-img/logoMain.png";
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <header className="header bg-black py-4 px-8">
       <nav className="flex flex-col items-center justify-between w-full">
         <div className="flex items-center justify-between w-full gap-8">
-          <img src={logoMain} alt="Logo" className="h-12 w-auto" />
+          <Link to={"/"}>
+            <img src={logoMain} alt="Logo" className="h-12 w-auto" />
+          </Link>
           <ul className="flex cursor-pointer items-center">
-            <li className="nav-item text-white font-semibold px-6 py-2 text-[16px]">
+            <Link
+              to={"/soporte"}
+              className="nav-item text-white font-semibold px-6 py-2 text-[16px]"
+            >
               Soporte
-            </li>
+            </Link>
             <li className="nav-item text-white font-semibold px-6 py-2 text-[16px]">
               Registrarse/Ingresar
             </li>
