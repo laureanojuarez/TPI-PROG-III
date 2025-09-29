3d54857dd160612478e76014295d38ec289c98e4
Laureano <laureanojuarez69@gmail.com>
2025-09-29 14:12:28 -0300

    refactor: update auth routes to remove token verification from register endpoint and improve profile component structure

--- frontend/src/pages/profile/profile.jsx
+++ frontend/src/pages/profile/profile.jsx
@@ -1,9 +1,9 @@
-export const Profile = () => {
+export default function Profile() {
   return (
     <>
-      <h1>Bienvenido {username}</h1>
+      <h1>Bienvenido Laureano Juarez</h1>
       <h1>Tus eventos</h1>
       <p>Evento: </p>
     </>
   );
-};
+}
