466022f15d73ffa3e356d82f84fbc6b0733734d7
Lucas <lucasoelschlager@gmail.com>
2025-10-08 11:37:30 -0300

    Cambios en search y navbar

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,11 +1,10 @@
-
 import { EventHeroList } from "../../components/Events/Events";
 import { Search } from "../../components/searchComponent/Search";
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center bg-gray-100 ">
+      <Search />
       <section className="flex flex-col w-full  gap-4 px-4 py-2">
-        <Search />
         <EventHeroList />
       </section>
     </main>
