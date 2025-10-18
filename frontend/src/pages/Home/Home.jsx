688d109da95e5436e79cba3c3e5fedfb1b6587ae
Laureano <laureanojuarez69@gmail.com>
2025-10-18 17:26:19 -0300

    feat: restructure event components; remove Events component, add EventSection for better event handling and loading states

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,31 +1,10 @@
-import {EventCard} from "../../components/Events/Events";
-import {Search} from "../../components/searchComponent/Search";
-import {useEvents} from "../../hooks/useEvents";
+import { EventSection } from "../../components/Events/EventSection";
+import { Search } from "../../components/searchComponent/Search";
 export default function Home() {
-  const {events, loading} = useEvents();
-
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100 ">
+    <div className="flex flex-col items-center justify-center">
       <Search />
-      <section className="flex flex-col w-full  gap-4 px-4 py-2">
-        {loading ? (
-          <div className="h-96 flex items-center justify-center rounded-md">
-            <p className="text-center text-gray-700 ">Cargando eventos...</p>
-          </div>
-        ) : events.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {events.map((event) => (
-              <EventCard key={event.id} event={event} />
-            ))}
-          </div>
-        ) : (
-          <div className="h-96 flex items-center justify-center rounded-md">
-            <p className="text-center text-gray-700 ">
-              No hay eventos disponibles.
-            </p>
-          </div>
-        )}
-      </section>
+      <EventSection />
     </div>
   );
 }
